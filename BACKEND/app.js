2848801b3b07d688cc7ff90c92bcd432e55e6e32
gidemho <135678932+gidemho@users.noreply.github.com>
2024-10-19 17:58:50 +0100

    set server

---
+++ BACKEND/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+//Load environment variables
+dotenv.config();
+
+//Connect to MongoDB
+connectDB();
+
+//Initialize the Express app
+const app = express();
+
+//Middleware to parse incoming JSON request
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('welcome to De_Recipez!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('PORT is alive');
+});
