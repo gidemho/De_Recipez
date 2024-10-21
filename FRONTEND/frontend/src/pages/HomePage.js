3a9c8b08c9f0a43117df59faf36d0c20c6e79f02
gidemho <135678932+gidemho@users.noreply.github.com>
2024-10-21 22:29:27 +0100

    add homepage

---
+++ FRONTEND/frontend/src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import RecipeList from '../components/RecipeList';
+import SearchBar from '../components/SearchBar';
+import { set } from 'mongoose';
+
+function HomePage() {
+    const {recipes, setRecipes} = useState([]);
+  
+    useEffect(() => {
+        axios.get('/api/recipes')
+        .then(response => setRecipes(response.data))
+        .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>De_Recipez</h1>
+            <SearchBar />
+            RecipeList recipes={recipes} />
+        </div>
+    );
+}
+
+export default HomePage;
