39c148638a7355109416ddc2b809202abf0bebee
gidemho <135678932+gidemho@users.noreply.github.com>
2024-10-22 11:07:14 +0100

    add recipe model

---
+++ BACKEND/models/Recipe.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const RecipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    steps: {
+        type: [String],
+        require: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
