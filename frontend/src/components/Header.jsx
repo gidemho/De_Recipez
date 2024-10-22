f3fbe71e13e01a15c2774040305df0e7c1e6019b
hashkitti001 <iJeddy5.0@gmail.com>
2024-10-22 22:33:28 +0100

    add basic structure for the header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className='shadow-md p-2 flex'>
-        <span className="logo">
-            <h2 className='text-md'>De Recipez</h2>
-        </span>
+    <header className='shadow-md flex p-2 w-full'>
+      <span className="logo p-2 w-2/12">
+        <h2 className='text-xl font-bold'>De Recipez</h2>
+      </span>
 
-        <nav className='flex'>
-            <ul className='flex'>
-                <li>Home</li>
-                <li>Recipes</li>
-                <li>About Me</li>
-            </ul>
-        </nav>
+      <div className='flex justify-between w-10/12'>
+      <nav className='p-2'>
+        <ul className='flex gap-8'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/recipes">Recipes</Link></li>
+          <li><Link to="/about-us">About Us</Link></li>
+        </ul>
+      </nav>
 
-        <button> Login </button>
+
+      <button className='p-3 bg-purple-500 text-white rounded-lg'> Login </button>
+      </div>
     </header>
   )
 }
